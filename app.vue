43a5eaeb31748076b93ba99e3ede9d4803bcbe32
Robinho0413 <101249228+Robinho0413@users.noreply.github.com>
2024-07-24 19:33:26 +0200

    First test

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
-<template>
+<!-- <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+  </div>
+</template> -->
+<template>
+  <div>
+   <h1 class="text-green-700">Welcome to the Fox</h1>
   </div>
 </template>
